abbe9c1c89e0660eaaf1608786f6d780faf9e960
noear <noear@live.cn>
2021-03-02 14:00:55 +0800

    1.0

--- solondoc-swagger2/src/main/java/org/noear/solondoc/swagger2/SwaggerController.java
+++ solondoc-swagger2/src/main/java/org/noear/solondoc/swagger2/SwaggerController.java
@@ -7,18 +7,14 @@ import org.noear.solon.annotation.Mapping;
 @Controller
 public class SwaggerController {
     @Mapping(value = "/swagger-resources", produces = "application/json")
-    public String resources() {
+    public String swagger_resources() {
         return new ONode().asArray().build(n -> {
             n.addNew().set("name", "应用接口")
-                    .set("location", "/v2/api-docs")
+                    .set("location", "/v2/swagger.json")
                     .set("swaggerVersion", "2.0");
         }).toJson();
     }
 
-    @Mapping("/v2/api-docs")
-    public String api_docs(){
-        return ONode.stringify(XPluginImp.swagger);
-    }
 
     @Mapping("/v2/swagger.json")
     public String swagger_json(){
